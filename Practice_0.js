82da674f2438f49beafe1d661cd33ab7fb2bf356
maahin2005 <mahin.malek2005@gmail.com>
2024-07-28 20:35:37 +0530

    Started the learning of TypeScript

---
+++ Practice_0.js
@@ -0,0 +1,7 @@
+console.log("First Day of TypeScript");
+var num1 = 5;
+console.log(num1);
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(5, 6));
---
+++ Practice_0.ts
@@ -0,0 +1,12 @@
+//@ts-ignore
+console.log("First Day of TypeScript");
+
+let num1: number = 5;
+
+console.log(num1);
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(sum(5, 6));
